4dd3ccfcf11f5e743ebc04c3e7e1f92c76210c34
dev34250 <dev.sharma.bis@gmail.com>
2024-01-28 03:34:25 +0530

    Initial Commit

--- app/src/main/java/com/example/paymnt/Payment.kt
+++ app/src/main/java/com/example/paymnt/Payment.kt
@@ -1,6 +1,7 @@
 package com.example.paymnt
 
 //import androidx.compose.material.icons.filled.CreditCard
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -16,16 +17,15 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -39,12 +39,12 @@ fun CheckoutScreen() {
     ) {
         IconButton(onClick = { }) {
             Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
                 contentDescription = null,
                 tint = Color.Black,
                 modifier = Modifier
                     .size(24.dp)
-                    .background(color = Color.Cyan)
+                    .background(color = Color.Cyan, shape = CircleShape)
                     .border(1.dp, Color.Blue, shape = CircleShape)
             )
         }
@@ -71,7 +71,7 @@ fun CheckoutScreen() {
             textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
+        TextField(
             value = "",
             onValueChange = { },
             label = { Text("Enter Amount") },
@@ -92,19 +92,19 @@ fun CheckoutScreen() {
         )
         Spacer(modifier = Modifier.height(30.dp))
         PaymentOption(
-            icon = Icons.Default.Home,
+            icon = R.drawable.img_1,
             title = "Metamask",
             description = ""
         )
         Spacer(modifier = Modifier.height(45.dp))
         PaymentOption(
-            icon = Icons.Default.Home,
+            icon = R.drawable.img_2,
             title = "Credit/Debit/ATM Card",
             description = "Add and secure cards, as per RBI guidelines"
         )
         Spacer(modifier = Modifier.height(45.dp))
         PaymentOption(
-            icon = Icons.Default.Home,
+            icon = R.drawable.img,
             title = "Net Banking",
             description = ""
         )
@@ -112,15 +112,14 @@ fun CheckoutScreen() {
 }
 
 @Composable
-fun PaymentOption(icon: ImageVector, title: String, description: String) {
+fun PaymentOption(icon: Int, title: String, description: String) {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = icon,
+        Image(
+            painter = painterResource(icon),
             contentDescription = null,
-            tint = Color.Black,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier.size(32.dp)
         )
         Spacer(modifier = Modifier.width(16.dp))
         Column {
